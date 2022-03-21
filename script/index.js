3248c900e4a0d0ffed1e27216d7c4436a145d4c0
Евгений Моисеев <eugen.moiseev@gmail.com>
2022-03-22 01:23:54 +0300

    Отработаны комментарии по результатам ревью 1

--- script/index.js
+++ script/index.js
@@ -1,31 +1,28 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit');
-let closeButton = document.querySelector('.popup__close-button');
-let saveButton = document.querySelector('.popup__save-button');
+let closeButton = popup.querySelector('.popup__close-button');
 let textTitle = document.querySelector('.profile__name');
 let textAbout = document.querySelector('.profile__about');
-let formElement = popup.querySelector('.popup__container');
-let inputName = popup.querySelector('.popup__input_name');
-let inputAbout = popup.querySelector('.popup__input_about');
+let formElement = popup.querySelector('.popup__container-form');
+let inputName = popup.querySelector('.popup__input_type_name');
+let inputAbout = popup.querySelector('.popup__input_type_about');
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
 function openPopup() {
-  popup.classList.add('popup_opened');
   inputName.value = textTitle.textContent;
   inputAbout.value = textAbout.textContent;
+  popup.classList.add('popup_opened');
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-  nameInput = popup.querySelector('.popup__input_name').value;
-  jobInput = popup.querySelector('.popup__input_about').value;
-  textTitle.textContent = nameInput;
-  textAbout.textContent = jobInput;
+  textTitle.textContent = inputName.value;
+  textAbout.textContent = inputAbout.value;
   closePopup();
 }
 
